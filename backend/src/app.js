2c594554efa28614fc6aa43d94c0da4054cd8a8a
Abhisek Das <abhisek.das@intglobal.com>
2026-03-05 13:19:25 +0530

    Allow frontend url for prod

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,9 +5,22 @@ const sequelize = require("./config/database");
 
 const app = express();
 
-/* ===== GLOBAL MIDDLEWARE ===== */
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://tanurima-1.onrender.com"
+];
+
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: function (origin, callback) {
+    // allow requests with no origin (mobile apps, curl, etc.)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
